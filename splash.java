440f8011b778015dc7f42e26f588cd46b7ab0db0
William <wdcherry03@gmail.com>
2024-09-18 14:07:27 -0500

    testing still

--- splash.java
+++ splash.java
@@ -1,10 +1,9 @@
 public class splash{
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        System.out.println("Hello world");
         //
-
-
+ 
 
     }
 }
