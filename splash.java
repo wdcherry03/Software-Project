e77c74097e667bb799fe64af0746b858e68204ef
William <wdcherry03@gmail.com>
2024-09-19 09:49:02 -0500

    editing splash file

--- splash.java
+++ splash.java
@@ -1,9 +1,29 @@
+//Splash Screen File
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 public class splash{
+    
+    private BufferedImage logo;
+
+    void loadImage()
+    {
+        try
+        {
+            this.logo = ImageIO.read(new File("logo.jpg"));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
     public static void main(String[] args)
     {
         System.out.println("Hello  world");
-        //
- 
-
     }
 }
