e057e59a641a6a0645d7e250d157fcd8ce384404
Wolla98 <winstonbounsavy@gmail.com>
2024-09-29 12:38:17 -0500

    Added database test code to test inserting and retrieval

--- Model.java
+++ Model.java
@@ -14,17 +14,35 @@ public class Model {
 	public Model() {
 
 		// Sets up connection to database
-		String url = "jdbc:postgresql://localhost/players";
-		Properties props = new Properties();
-		props.setProperty("user", "student");
-		props.setProperty("password", "student");
+		String url = "jdbc:postgresql://localhost/photon";
 		Connection database;
 
 		try {
-			database = DriverManager.getConnection(url, props);
+			// Connects to the database
+			database = DriverManager.getConnection(url, "student", "student");
+
+			// Attempts to add two players to the database
+			Statement st = database.createStatement();
+			st.executeUpdate("INSERT INTO players (id, codename) VALUES (5, 'wolfgang')");
+
+			st = database.createStatement();
+			st.executeUpdate("INSERT INTO players (id, codename) VALUES (6, 'griffin')");
+
+			// Attempts to retrieve players from the database
+			st = database.createStatement();
+			ResultSet rs = st.executeQuery("SELECT * FROM players");
+
+			// Prints players as a test
+			System.out.println("Database Retrieved Players: ");
+			while (rs.next()) {
+					System.out.println(rs.getString(2));
+			}
+			rs.close();
+			st.close();
 		} 
 		catch (SQLException e) {
 			System.out.println("Error connecting to database, skipping connection step");
+			System.out.println(e.getMessage());
 		}
 	}
 
