36e7c3ec6093296184b6f587cf521bf02820a0b0
Logan <yoboysharp@gmail.com>
2024-09-29 21:45:03 -0500

    Added Player.java

---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player 
+{
+   public Player(int PlayerID, String Codename, int HardwareID)
+   {
+      int playerId = PlayerID;
+      String codename = Codename;
+      int hardwareID = HardwareID;
+   }
+}
