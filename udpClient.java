d521fc67fb5a9e6b459889ea0f8aa67af4bc40cd
Logan <yoboysharp@gmail.com>
2024-09-29 21:16:46 -0500

    Added constructor and update functions to UDP files

--- udpClient.java
+++ udpClient.java
@@ -4,9 +4,69 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class udpClient
 {
+	// Constructor
+	public udpClient()
+	{
+		try {
+			Scanner sc = new Scanner(System.in);
+
+			// Step 1:Create the socket object for carrying the data. (port 7500)
+			DatagramSocket ds = new DatagramSocket(7500);
+		}
+		catch (SocketException e) {
+			System.out.println("ERROR creating socket");
+			System.out.println(e.getMessage());
+		}
+
+		try {
+			InetAddress ip = InetAddress.getLocalHost();
+			//InetAddress ip = InetAddress.getByName("192.168.1.100"); // Address given in slides
+		}
+		catch (UnknownHostException e) {
+			System.out.println("ERROR connecting to host");
+			System.out.println(e.getMessage());
+		}
+		byte[] buf = null;
+	}
+
+	// Update function, runs every frame
+	public int update(Scanner SC, DatagramSocket DS, byte[] B, InetAddress IP)
+	{
+		Scanner sc = SC;
+		DatagramSocket ds = DS;
+		byte[] buf = B;
+		InetAddress ip = IP;
+
+		String inp = sc.nextLine();
+
+		// convert the String input into the byte array.
+		buf = inp.getBytes();
+
+		// Step 2 : Create the datagramPacket for sending the data.
+		DatagramPacket DpSend = new DatagramPacket(buf, buf.length, ip, 7501);
+
+		try {
+			// Step 3 : invoke the send call to actually send the data.
+			ds.send(DpSend);
+		}
+		catch (IOException e) {
+			System.out.println("ERROR sending data");
+			System.out.println(e.getMessage());
+		}
+
+		// break the loop if user enters "bye"
+		if (inp.equals("bye"))
+			return 1; // Returns 1 when exiting
+		return 0;
+	}
+}
+
+/*
 	public static void main(String args[]) throws IOException
 	{
 		Scanner sc = new Scanner(System.in);
@@ -37,4 +97,4 @@ public class udpClient
 				break;
 		}
 	}
-}
+*/
--- udpServer.java
+++ udpServer.java
@@ -7,6 +7,71 @@ import java.net.SocketException;
 
 public class udpServer
 {
+	// Costructor
+	public udpServer() throws SocketException
+	{
+		try {
+			// Step 1 : Create a socket to listen at port 7501
+			DatagramSocket ds = new DatagramSocket(7501);
+			byte[] receive = new byte[65535];
+			
+			DatagramPacket DpReceive = null;
+		} 
+		catch (SocketException e) {
+			System.out.println("ERROR creating socket");
+			System.out.println(e.getMessage());
+		}
+	}
+
+	// Update function, runs every frame
+	public int update(DatagramPacket DP, byte[] R, DatagramSocket DS)
+	{
+		// Step 2 : create a DatgramPacket to receive the data.
+		DatagramPacket DpReceive = DP;
+		byte[] receive = R;
+		DpReceive = new DatagramPacket(receive, receive.length);
+
+		DatagramSocket ds = DS;
+		try {
+			// Step 3 : recieve the data in byte buffer.
+			ds.receive(DpReceive);
+		}
+		catch (IOException e) {
+			System.out.println("ERROR receiving data");
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Client:-" + data(receive));
+
+		// Exit the server if the client sends "bye"
+		if (data(receive).toString().equals("bye"))
+		{
+			System.out.println("Client sent bye.....EXITING");
+			return 1; // Returns 1 when exiting
+		}
+
+		// Clear the buffer after every message.
+		receive = new byte[65535];
+		return 0;
+	}
+
+	// A utility method to convert the byte array
+	// data into a string representation.
+	public static StringBuilder data(byte[] a)
+	{
+		if (a == null)
+			return null;
+		StringBuilder ret = new StringBuilder();
+		int i = 0;
+		while (a[i] != 0)
+		{
+			ret.append((char) a[i]);
+			i++;
+		}
+		return ret;
+	}
+}
+
+/*
 	public static void main(String[] args) throws IOException
 	{
 		// Step 1 : Create a socket to listen at port 7501
@@ -36,20 +101,4 @@ public class udpServer
 			receive = new byte[65535];
 		}
 	}
-
-	// A utility method to convert the byte array
-	// data into a string representation.
-	public static StringBuilder data(byte[] a)
-	{
-		if (a == null)
-			return null;
-		StringBuilder ret = new StringBuilder();
-		int i = 0;
-		while (a[i] != 0)
-		{
-			ret.append((char) a[i]);
-			i++;
-		}
-		return ret;
-	}
-}
+*/
