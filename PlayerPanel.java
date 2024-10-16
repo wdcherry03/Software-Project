21e769df53675fe72fe106a5930ba2f9c030edbe
Wolla98 <winstonbounsavy@gmail.com>
2024-10-15 21:05:21 -0500

    Fixed data fields being in the wrong column (again)

--- PlayerPanel.java
+++ PlayerPanel.java
@@ -33,8 +33,8 @@ public class PlayerPanel extends JPanel {
         playerHardwareID.setEditable(false);
 
         // Adds them to this panel
-        this.add(playerID);
         this.add(playerCodename);
+        this.add(playerID);
         this.add(playerHardwareID);
     }
 }
