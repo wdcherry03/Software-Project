4a125eae745a83ebd4f9c0f5f244648e5f6e6873
William <wdcherry03@gmail.com>
2024-11-24 23:02:03 -0600

    Testing f7 functionality

--- Controller.java
+++ Controller.java
@@ -62,7 +62,7 @@ public class Controller implements ActionListener, MouseListener, KeyListener {
 			case KeyEvent.VK_F5:
 				view.resetGameState(); // Sets 'gameStart' and 'gameEnd' to false
 				view.runGame(); break; // Go to gameplay screen
-			case KeyEvent.VK_F7: view.runEntry(); view.clearEntries(); break; // Return to entry screen and clear arraylists
+			case KeyEvent.VK_F7: view.clearEntries(); view.runEntry(); break; // Return to entry screen and clear arraylists
 
 			// UDP Debugging tools
 			// case KeyEvent.VK_F2: view.eventOccured(13, 14); System.out.println("Event recieved"); break;		
